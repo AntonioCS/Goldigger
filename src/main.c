c061dec3a2f64818f29078cbce1d38bd4a92bea7
AntonioCS <antoniocs@gmail.com>
2018-06-03 11:57:28 +0100

    Removed includes that were no longer needed

--- src/main.c
+++ src/main.c
@@ -1,19 +1,10 @@
-#include <stdio.h>
-
-#if defined(__APPLE__)
-#include "SDL.h"
-#include "SDL_image.h"
-#else
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
-#endif
-//#include "SDL_mixer.h"
-//#include "SDL2/SDL_ttf.h"
+
+
 #include "GameSDL.h"
 #include "GameState.h"
-#include "MapObject.h"
-#include "module.h"
-#include "player.h"
+
 
 int main(int argc, char* argv[])
 {
